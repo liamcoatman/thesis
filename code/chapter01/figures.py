2769f0b17e54da21c664dc530751b3b3fa323acd
Liam Coatman <liam@user-43-25.vpn.ast.private.cam.ac.uk>
2017-08-22 14:39:18 +0100

    viva corrections

--- code/chapter01/figures.py
+++ code/chapter01/figures.py
@@ -3,39 +3,40 @@ def shang_sed():
     import matplotlib.pyplot as plt
     import numpy as np
     from scipy.constants import c
+    from PlottingTools.plot_setup_thesis import figsize, set_plot_properties
+
+    set_plot_properties() # change style 
     
     rltab = np.genfromtxt('rlmsedMR.txt')
-    rqtab = np.genfromtxt('rqmsedMR.txt')
+
+    fig, ax = plt.subplots(figsize=figsize(1.0, vscale=0.8))
     
-    fig = plt.figure(figsize=(7,5))
-    ax = fig.add_subplot(111)
-    
-    ax.plot( 1.0e10 * c / (10**rltab[:,0]), 10**rltab[:,1], color='black', label='Radio-loud' )
-    ax.plot( 1.0e10 * c / (10**rqtab[:,0]), 10**rqtab[:,1], color='green', label='Radio-quiet' )
-    
-    ax.axvline(912,color='black',linestyle='--')
-    ax.axvline(10**4,color='black',linestyle='--')
-    ax.axvline(6.e5,color='black',linestyle='--')
-    
-    ax.set_xlabel(r'Wavelength $\AA$',fontsize=10)
-    ax.set_ylabel( r'log(${\lambda}f_{\lambda}$) (Arbitrary Units)',fontsize=10)
+    ax.plot(1.0e10 * c / (10**rltab[:,0]), 10**rltab[:,1], color='black', lw=1)
+  
+    ax.set_xlabel(r'Wavelength [\AA]')
+    ax.set_ylabel( r'${\lambda}f_{\lambda}$ [Arbitrary Units]')
     
     ax.set_xscale('log')
     ax.set_yscale('log')
     
-    ax.set_ylim(1e-3,12)
-    ax.set_xlim(10,10**8)
-    
-    plt.legend( loc='lower left', prop={'size':10} )
-    
-    ax.text(1.3e3, 2e-1, 'Big Blue \n Bump', fontsize=12)
-    ax.text(2e4, 2e-1, 'Near-IR Bump', fontsize=12)
-    
-    plt.tick_params(axis='both',which='major',labelsize=10)
+    ax.set_ylim(1e-2, 10)
+    ax.set_xlim(10, 10**8)
+
+    ax.text(0.22, 0.3, 'Accretion \n disc', transform = ax.transAxes, multialignment='center')
+    ax.text(0.52, 0.8, 'Torus', transform = ax.transAxes, multialignment='center')
+    ax.text(0.1, 0.85, 'BLR \& \n NLR', transform = ax.transAxes, multialignment='center')
+
+    ax.arrow(0.30, 0.4, 0.0, 0.2, color='black', transform = ax.transAxes, head_width=0.015 )
+    ax.arrow(0.22, 0.88, 0.04, 0.0, color='black', transform=ax.transAxes, head_width=0.015)
+    ax.arrow(0.57, 0.78, 0.0, -0.06, color='black', transform=ax.transAxes, head_width=0.015)
+
     plt.tight_layout()
-    
-    plt.savefig('/home/lc585/thesis/figures/chapter01/shangsed.pdf')
+
+    plt.savefig('../../figures/chapter01/shangsed.pdf')
 
     plt.show() 
     
-    return None 
+    return None
+
+if __name__ == '__main__':
+    shang_sed()
